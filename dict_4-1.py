e6ec421522b887c085fac7182a0d5907c105f0e0
Влад <setlefo.gmail.com>
2020-02-17 02:24:48 +0200

    initial commit

---
+++ dict_4-1.py
@@ -0,0 +1,18 @@
+#Import library
+import json
+
+# This is a python library for 'Text Processing Serveices', as the offcial site suggests.
+import difflib
+from difflib import SequenceMatcher
+
+#Let's load the same data again
+data = json.load(open("dictionary.json"))
+
+#Run a Sequence Matcher
+#First parameter is 'Junk' which includes white spaces, blank lines and so onself.
+#Second and third parameters are the words you want to find similarities in-between.
+#Ratio is used to find how close those two words are in numerical terms
+value = SequenceMatcher(None, "cattb", "cat").ratio()
+
+#Print out the value
+print(value)
